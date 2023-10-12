f64305c52a6cb0125b8196915b0f18b02b7f0e29
sivadharshinisiva <144514091+sivadharshinisiva@users.noreply.github.com>
2023-10-12 11:20:09 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,7 +1,25 @@
-def rfact(x):
-  f=1
-  if(x==0):
-    return(1)
-    f=x*rfact(x-1)
-    return(f)
-    printf("Factorial is"+str(rfact(5)))
+class Student:
+  def __init__(self, name, roll_no, cgpa):
+    self.name = name
+    self.roll_no =roll_no
+    self.cgpa =cgpa
+
+def sort_students(student_list):
+  sorted_students=sorted(student_list,key=lambda student:student.cgpa, reverse=True)
+
+  return sorted_students
+
+students=[Student("student1","001",7.0),
+         Student("student2","002",8.0),
+         Student("student3","003",6.0),
+         Student("student4","004",9.0),
+         Student("student5","005",9.8),
+         ]
+
+sorted_students=sort_students(students)
+
+for student in sorted_students:
+  print("Name:{} Roll_no:{} CGPA:{}".format(student.name,student.roll_no,student.cgpa))
+  
+  
+
--- main (3).py
+++ main (3).py
@@ -1,5 +1,12 @@
-y=int(input("Enter valid year"))
-if(y%4==0):
-  print("Given year is a leap year")
-else:
-  print("Given year is not a leap year")
+def linear_search_product(productlist,tar):
+  indices=[]
+  for index, product in enumerate(productlist):
+    if product==tar:
+      indices.append(index)
+
+  return indices
+
+product=["rice","nuts","dals","nuts","fruits","vegetable"]
+target="nuts"
+result=linear_search_product(product,target)
+print(result)
